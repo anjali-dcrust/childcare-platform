34961028c0c79095311e66bd35679bb01cdc7f59
Anjali <anjalisaini172005@gmail.com>
2026-07-20 23:37:53 +0530

    feat: add OpenTelemetry auto-instrumentation with Jaeger tracing

---
+++ run-with-otel.ps1
@@ -0,0 +1,9 @@
+﻿$env:JAVA_TOOL_OPTIONS="-javaagent:./opentelemetry-javaagent.jar"
+$env:OTEL_SERVICE_NAME="childcare-platform"
+$env:OTEL_EXPORTER_OTLP_ENDPOINT="http://localhost:4318"
+$env:OTEL_EXPORTER_OTLP_PROTOCOL="http/protobuf"
+$env:OTEL_TRACES_EXPORTER="otlp"
+$env:OTEL_METRICS_EXPORTER="none"
+$env:OTEL_LOGS_EXPORTER="none"
+
+mvn spring-boot:run
